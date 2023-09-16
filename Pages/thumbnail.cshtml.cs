e276150fd0a06e6f6de477c964a54d738c2e1d6c
aavocadobby <92611490+aavocadobby@users.noreply.github.com>
2023-09-16 13:50:13 -0400

    updated css

---
+++ Pages/thumbnail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ciao_bella.Pages
+{
+    public class thumbnailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
